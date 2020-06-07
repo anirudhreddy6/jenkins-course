4a2a457e6ac717f87e2c09b1c911085177e151be
Anirudh Reddy Thumallapally <anirudhreddy3231@Anirudhs-MBP-2.attlocal.net>
2020-06-06 23:17:42 -0500

    new

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('anirudhreddy6')
+            node / gitConfigEmail('anirudhreddy06@gmail.com')
         }
     }
     triggers {
